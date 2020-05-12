504bfac8fc8fb3eb75127fdc94cdb9ad01a60823
Jens Linkekamp <jens.lindekamp@ista.com>
2020-05-12 10:15:03 +0200

    Added button and afterViewInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
   presetName = 'meter';
 
@@ -54,8 +54,19 @@ export class AppComponent implements OnInit {
     ],
   };
 
+  constructor(private ngZone: NgZone) {}
+
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
     this.initAnyline();
+  }
+
+  initAnylineOutsideAngular() {
+    this.ngZone.runOutsideAngular(() => {
+      this.initAnyline();
+    });
   }
 
   initAnyline() {
